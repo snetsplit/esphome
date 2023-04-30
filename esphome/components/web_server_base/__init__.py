65129489bbb5852f5c23a20670b86eb82fc18050
snetsplit <snetsplit@gmail.com>
2023-04-30 13:46:37 -0500

    Revert "Update __init__.py"
    This reverts commit 27adf939244ef3212e370e5949bf214c5c30f4d2.

--- esphome/components/web_server_base/__init__.py
+++ esphome/components/web_server_base/__init__.py
@@ -28,6 +28,7 @@ async def to_code(config):
         cg.add_library("FS", None)
         cg.add_library("Update", None)
     # https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
+    cg.add_library("esphome/ESPAsyncWebServer-esphome", "2.1.0")
         cg.add_library(
                 name="ESPAsyncWebServer",
                 repository="https://github.com/snetsplit/ESPAsyncWebServer",
